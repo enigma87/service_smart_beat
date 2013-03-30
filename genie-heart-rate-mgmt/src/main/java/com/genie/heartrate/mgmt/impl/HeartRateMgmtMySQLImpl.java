1fa838ea0a4bd96493d911ae939d15061bc1dacf
manojkumar <manoj1987@gmail.com>
2013-03-30 10:27:21 -0700

    MySQL impl for Interface

---
+++ genie-heart-rate-mgmt/src/main/java/com/genie/heartrate/mgmt/impl/HeartRateMgmtMySQLImpl.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.genie.heartrate.mgmt.impl;
+
+import com.genie.heartrate.mgmt.beans.HeartRate;
+import com.genie.heartrate.mgmt.core.HeartRateMgmt;
+
+/**
+ * @author manojkumar
+ *
+ */
+public class HeartRateMgmtMySQLImpl implements HeartRateMgmt 
+{
+
+	public HeartRate getHeartRateForUser(Long userid) 
+	{
+		return null;
+	}
+
+	public void createHeartRateForUser(HeartRate heartRate) 
+	{
+		
+	}
+
+	public void updateHeartRateForUser(HeartRate heartRate) 
+	{
+		
+	}
+
+}
