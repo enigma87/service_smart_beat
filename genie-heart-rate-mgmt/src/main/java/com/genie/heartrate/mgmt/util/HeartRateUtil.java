fde9bbc47519c7d343effb9b0a512ca3f29cda87
vidhunkps <vidhunkps@yahoo.co.in>
2013-04-03 23:03:16 +0530

    Implemented the calculateHeartrateZones Method under HeartRateUtil Class. Also created unit test case for the same.

--- genie-heart-rate-mgmt/src/main/java/com/genie/heartrate/mgmt/util/HeartRateUtil.java
+++ genie-heart-rate-mgmt/src/main/java/com/genie/heartrate/mgmt/util/HeartRateUtil.java
@@ -3,6 +3,7 @@
  */
 package com.genie.heartrate.mgmt.util;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,7 +24,29 @@ public class HeartRateUtil
 	
 	public static UserHeartRateZone calculateHeartRateZones(UserHeartRateTest userheartRateTest)
 	{
-		//TODO Calculate the HRZ's
-		return new UserHeartRateZone();
+		UserHeartRateZone userHeartRateZone = new UserHeartRateZone();
+		
+		final Integer restingHr   = userheartRateTest.getRestingHr();
+		final Integer thresholdHr = userheartRateTest.getThresholdHr();
+		final Integer maximalHr   = userheartRateTest.getMaximalHr();
+		final Integer hrReserve   = maximalHr-restingHr;
+		
+		userHeartRateZone.setUserid(userheartRateTest.getUserid());
+		userHeartRateZone.setHrz1Start((double) restingHr);
+		userHeartRateZone.setHrz1End((double)((thresholdHr-restingHr)/2+restingHr));
+		userHeartRateZone.setHrz2Start(userHeartRateZone.getHrz1End()+0.01);
+		userHeartRateZone.setHrz2End((thresholdHr-userHeartRateZone.getHrz1End())/2+userHeartRateZone.getHrz1End());
+		userHeartRateZone.setHrz4Start(thresholdHr-(0.04*hrReserve));
+		userHeartRateZone.setHrz4End(thresholdHr+(0.02*hrReserve));
+		userHeartRateZone.setHrz3Start(userHeartRateZone.getHrz2End()+0.01);
+		userHeartRateZone.setHrz3End(userHeartRateZone.getHrz4Start()-0.01);
+		userHeartRateZone.setHrz6Start(maximalHr-(0.06*hrReserve));
+		userHeartRateZone.setHrz6End((double)maximalHr);
+		userHeartRateZone.setHrz5Start(userHeartRateZone.getHrz4End()+0.01);
+		userHeartRateZone.setHrz5End(userHeartRateZone.getHrz6Start()-0.01);
+		userHeartRateZone.setCreatedTs(Calendar.getInstance());
+		userHeartRateZone.setUpdatedTs(Calendar.getInstance());
+		
+		return userHeartRateZone;
 	}
 }
