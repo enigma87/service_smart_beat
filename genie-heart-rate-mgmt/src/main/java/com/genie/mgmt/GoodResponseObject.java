14342978da5b1c3cd4217d97dc3f6f7dadfe9d4e
dhas <dhasarathy@gmail.com>
2013-04-16 20:22:29 +0200

    pojo changes

---
+++ genie-heart-rate-mgmt/src/main/java/com/genie/mgmt/GoodResponseObject.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.genie.mgmt;
+
+/**
+ * @author manojkumar
+ *
+ */
+public class GoodResponseObject 
+{
+	public int status;
+	public String message;
+	public Object obj;
+	
+	public GoodResponseObject(int status, String message)
+	{
+		this.status = status;
+		this.message = message;
+	}
+	
+	public GoodResponseObject(int status, String message, Object obj)
+	{
+		this.status = status;
+		this.message = message;
+		this.obj = obj;
+	}
+	
+	public int getStatus()
+	{
+		return this.status;
+	}
+	
+	public void setStatus(int status)
+	{
+		this.status = status;
+	}
+	
+	public String getMessage()
+	{
+		return this.message;
+	}
+	
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+	
+	public Object getObj()
+	{
+		return this.obj;
+	}
+	
+	public void setObj(Object obj)
+	{
+		this.obj = obj;
+	}
+}
