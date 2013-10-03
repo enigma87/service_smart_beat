c60593f31aa263c040d293a6ca1ed6cd65c123f7
vidhunkps <vidhunkps@yahoo.co.in>
2013-10-03 22:20:09 +0530

    Invalid Time Exceptions

---
+++ genie-smartbeat/src/main/java/com/genie/smartbeat/core/exceptions/time/InvalidEndTimestampException.java
@@ -0,0 +1,13 @@
+package com.genie.smartbeat.core.exceptions.time;
+
+/**
+ * @author Vidhun
+ **/
+
+public class InvalidEndTimestampException extends InvalidTimestampException{
+
+	private static final long serialVersionUID = -1249319979421292062L;
+	public InvalidEndTimestampException(){
+		super();
+	}
+}
---
+++ genie-smartbeat/src/main/java/com/genie/smartbeat/core/exceptions/time/InvalidStartTimestampException.java
@@ -0,0 +1,14 @@
+package com.genie.smartbeat.core.exceptions.time;
+
+
+/**
+ * @author Vidhun
+ **/
+public class InvalidStartTimestampException extends InvalidTimestampException{
+	
+	private static final long serialVersionUID = -1249319979421292061L;
+	
+	public InvalidStartTimestampException(){
+		super();
+	}
+}
